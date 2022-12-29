5d6f86b90c7f78534c2a653a6b24bdaefe983b46
Alexander R. Torrijos <artorrijos@gmail.com>
2022-12-29 21:26:42 +0800

    Multiply working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    print_multiply(4, 5);
+    print_multiply(3, 4);
+}
+
+/// A simple function example
+///
+/// # Examples
+///
+/// ```
+/// print_multiply(3, 5);
+///  
+/// ```
+
+fn print_multiply(x: i32, y: i32) {
+    println!("x * y = {}", x * y);
 }
